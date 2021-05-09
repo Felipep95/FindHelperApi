3cd16d40bb5a68bc53808b30e39b578207671139
Felipe Pereira <felipe.pereiralf95@gmail.com>
2021-05-09 00:10:44 -0300

    Created and configured FindHelperApiContext

---
+++ FindHelperApi/Data/FindHelperApiContext.cs
@@ -0,0 +1,19 @@
+﻿using FindHelperApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FindHelperApi.Data
+{
+    public class FindHelperApiContext : DbContext
+    {
+        public FindHelperApiContext(DbContextOptions<FindHelperApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<FriendRequest> FriendRequests { get; set; }
+        public DbSet<FriendList> FriendLists { get; set; }
+        public DbSet<Publication> Publications { get; set; }
+    }
+}
--- FindHelperApi/Startup.cs
+++ FindHelperApi/Startup.cs
@@ -1,7 +1,9 @@
+using FindHelperApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +28,11 @@ namespace FindHelperApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddDbContext<FindHelperApiContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("FindHelperApi"), builder =>
+                    builder.MigrationsAssembly("FindHelperApi")));
+            
+                services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FindHelperApi", Version = "v1" });
